e56082bf5d19452e10979e9f9df2a1949437f931
pheonixash266 <110310052+pheonixash266@users.noreply.github.com>
2023-04-01 19:48:23 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+
+function individualRoom() {
+document.getElementById("information"), position = "top";
+document.getElementById("information1"), position = "bottom";
+document.getElementById("information2"), position = "bottom";
+document.getElementById("room1").style.display = "block";
+document.getElementById("room2").style.display = "block";
+document.getElementById("room4"), position = "bottom";
+document.getElementById("room3"), position = "bottom";
+document.getElementById("room6"), position = "bottom";
+document.getElementById("room5"), position = "bottom";
+}
+function suiteRoom() {
+	document.getElementById("information"), position = "bottom";
+	document.getElementById("information1").style.display = "block";
+	document.getElementById("information2"), position = "bottom";
+	document.getElementById("room1"), position = "bottom";
+	document.getElementById("room2"), position = "bottom";
+	document.getElementById("room4").style.display = "block";
+	document.getElementById("room3").style.display = "block";
+	document.getElementById("room6"), position = "bottom";
+	document.getElementById("room5"), position = "bottom";
+}
+function familyRoom() {
+	document.getElementById("information").style.display = "none";
+	document.getElementById("information1").style.display = "none";
+	document.getElementById("information2").style.display = "block";
+	document.getElementById("room1").style.display = "none";
+	document.getElementById("room2").style.display = "none";
+	document.getElementById("room4").style.display = "none";
+	document.getElementById("room3").style.display = "none";
+	document.getElementById("room6").style.display = "block";
+	document.getElementById("room5").style.display = "block";
+}
